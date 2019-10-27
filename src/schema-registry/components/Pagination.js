806b50b23654488137dd3576fc40b06e1801bb88
Nikita Ryanov <ryanov.nikita@gmail.com>
2019-10-27 16:19:03 +0300

    [SCHEMAKEEPER-UI] Fix pagination

--- src/schema-registry/components/Pagination.js
+++ src/schema-registry/components/Pagination.js
@@ -7,13 +7,13 @@ const Pagination = ({page, maxPage, changePage}) => (
         <ul className="pagination justify-content-center">
             <li className={`page-item ${page === 1 ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(page - 1)}>Previous</button></li>
             <li className={`page-item ${page === 1 ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(1)}>1</button></li>
-            { page < 4 ? <></> : <li className="page-item"><button type="button" className="page-link">...</button></li>}
-            { page < 3 ? <></> : <li className="page-item"><button type="button" className="page-link">{page - 1}</button></li>}
-            { page === 1 || page === maxPage ? <></> : <li className="page-item disabled"><button type="button" className="page-link">{page}</button></li>}
-            { maxPage - 1 > 0 && page >= maxPage - 1 ? <></> : <li className="page-item"><button type="button" className="page-link">{page + 1}</button></li>}
-            { maxPage - 2 > 0 && page >= maxPage - 2 ? <></> : <li className="page-item"><button type="button" className="page-link">...</button></li>}
-            <li className={`page-item ${page === maxPage ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(maxPage)}>{maxPage}</button></li>
-            <li className={`page-item ${page === maxPage ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(page + 1)}>Next</button></li>
+            { page - 2 > 1 ? <li className="page-item"><button type="button" className="page-link">...</button></li> : <></> }
+            { page - 1 > 1 ? <li className="page-item"><button type="button" className="page-link">{page - 1}</button></li> :  <></>}
+            { page === 1 ? <></> : <li className="page-item disabled"><button type="button" className="page-link">{page}</button></li>}
+            { page + 1 < maxPage ? <li className="page-item"><button type="button" className="page-link">{page + 1}</button></li> : <></>}
+            { page + 2 < maxPage ? <li className="page-item"><button type="button" className="page-link">...</button></li> : <></>}
+            {maxPage === 0 || maxPage === page ? <></> : <li className={`page-item ${page === maxPage ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(maxPage)}>{maxPage}</button></li>}
+            <li className={`page-item ${maxPage === 0 || page === maxPage ? 'disabled' : ''}`}><button type="button" className="page-link" onClick={() => changePage(page + 1)}>Next</button></li>
         </ul>
     </nav>
 );
